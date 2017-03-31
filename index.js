9e104713c64a7d51055fb16ef77e83792e3130dd
乔杨 <peiqiao.ppq@alipay.com>
2017-03-31 16:59:15 +0800

    update

--- index.js
+++ index.js
@@ -1,42 +1,11 @@
 const fs = require('fs');
-const http = require('axios');
-const dataJSON = './data.json';
-const base = 'http://cn.bing.com';
+const path = require('path');
 
-http.get(base + '/HPImageArchive.aspx', {
-  params: {
-    format: 'js',
-    idx: 0,
-    n: 100
-  }
-}).then(res => {
-  return res.data.images.map(img => base + img.url)
-}).then(imgs => {
-  let dataStr = fs.readFileSync(dataJSON, { encoding: 'utf8' });
-  if (!dataStr) { dataStr = '{}'; }
-
-  const data = JSON.parse(dataStr);
-  if (!data.images) { data.images = [] }
-
-  const len = data.images.length;
-
-  imgs.forEach(url => {
-    if (data.images.indexOf(url) === -1) {
-      data.images.push(url);
-    }
-  })
-
-  console.log('update finished, count: ' + (data.images.length - len));
-
-  fs.writeFileSync(dataJSON, JSON.stringify(data, null, 2));
-
-  return data.images;
-}).then(imgs => {
-  const html = fs.readFileSync('./photos.html', { encoding: 'utf8' });
-  const ul = '<ul>\n' + imgs.map(url => `    <li><img src="${url}"></li>\n`).join('') + '  </ul>';
-  const newHtml = html.replace(/<ul>[^]*<\/ul>/i, ul);
-
-  fs.writeFileSync('./photos.html', newHtml);
-}).catch(ex => {
-  console.error(ex);
-})
+fs.readdirSync('./tasks').filter(fileName => path.extname(fileName) === '.js').forEach(file => {
+  const task = require(path.join(__dirname, './tasks', file));
+  console.log(`----< task begin >---- ${task.config.name}\n`);
+  const startDate = new Date();
+  task.run();
+  const costs = new Date() - startDate;
+  console.log(`----< task finished, costs: ${costs} ms >---- ${task.config.name}\n`);
+});
---
+++ tasks/fetchImg.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const http = require('axios');
+const base = 'http://cn.bing.com';
+const path = require('path');
+const dataJSON = path.join(__dirname, './data.json');
+
+exports.config = {
+  name: 'fetch images from bing.com'
+}
+
+exports.run = function () {
+
+  http.get(base + '/HPImageArchive.aspx', {
+    params: {
+      format: 'js',
+      idx: 0,
+      n: 100
+    }
+  }).then(res => {
+    return res.data.images.map(img => base + img.url)
+  }).then(imgs => {
+    let dataStr = fs.readFileSync(dataJSON, { encoding: 'utf8' });
+    if (!dataStr) { dataStr = '{}'; }
+
+    const data = JSON.parse(dataStr);
+    if (!data.images) { data.images = [] }
+
+    const len = data.images.length;
+
+    imgs.forEach(url => {
+      if (data.images.indexOf(url) === -1) {
+        data.images.push(url);
+      }
+    })
+
+    console.log('update finished, count: ' + (data.images.length - len));
+
+    fs.writeFileSync(dataJSON, JSON.stringify(data, null, 2));
+
+    return data.images;
+  }).then(imgs => {
+    const html = fs.readFileSync('./photos.html', { encoding: 'utf8' });
+    const ul = '<ul>\n' + imgs.map(url => `    <li><img src="${url}"></li>\n`).join('') + '  </ul>';
+    const newHtml = html.replace(/<ul>[^]*<\/ul>/i, ul);
+
+    fs.writeFileSync('./photos.html', newHtml);
+  }).catch(ex => {
+    console.error(ex);
+  })
+}
